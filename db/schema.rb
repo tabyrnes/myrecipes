2c7020374631e110c568506a92d4b382b7d0d637
tabyrnes <tabyrnes@gmail.com>
2015-12-18 10:42:00 +0000

    cloned and updated migration

--- db/schema.rb
+++ db/schema.rb
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 20151218094919) do
   end
 
   create_table "recipes", force: :cascade do |t|
+    t.string   "name"
+    t.text     "summary"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
