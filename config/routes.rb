2f16b817cf78891be28d898098889b60b33538d2
tabyrnes <tabyrnes@gmail.com>
2016-01-18 15:36:46 +0000

    added pagination

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   
   get '/home', to: 'pages#home'
   
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+  
+
   
   
 end
