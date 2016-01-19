fbefb871b6095c1458ff58e479b6e19e47f37db8
tabyrnes <tabyrnes@gmail.com>
2016-01-19 14:37:29 +0000

    added chef functionality

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     end
   end
   
+  resources :chefs, except: [:new]
+  
+  get '/register', to: 'chefs#new'
+  
 
   
   
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118141218) do
+ActiveRecord::Schema.define(version: 20160119130747) do
 
   create_table "articles", force: :cascade do |t|
   end
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160118141218) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
   create_table "likes", force: :cascade do |t|
